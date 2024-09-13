9c72335f8139d779f875eb1c499865d195a6ae66
Mark Mutaiti <mutaitimutisya@gmail.com>
2024-09-13 17:38:20 +0300

    Calculator functionality done.

--- script.js
+++ script.js
@@ -11,29 +11,76 @@ function multiplyNums(a,b){
 }
 
 function divideNums(a,b){
-    return round(a/b)
+    return Math.round(a/b)
+}
+
+function percentOf(a,b){
+    return Math.round(a%b)
 }
 
 
 const numDisplay = document.querySelector(".display")
 
-const numOne = document.querySelector(".one")
-const numTwo = document.querySelector(".two")
+const buttons = document.querySelectorAll(".num")
 
+const operands = document.querySelectorAll(".operand")
 
-numOne.addEventListener("click", () => {
-    numDisplay.textContent = numOne.textContent
+const equals = document.querySelector(".equals")
+
+const clear = document.querySelector(".clear")
+
+const del = document.querySelector(".delete")
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        numDisplay.textContent = numDisplay.textContent + button.textContent
+    })
 })
 
-numTwo.addEventListener("click", () => {
-    numDisplay.textContent = numTwo.textContent
+let operandArr = ["+", "-", "*", "/", "%"]
+
+let operandVar = ""
+
+operands.forEach(operand => {
+    operand.addEventListener("click", () => {
+        displayArrSplitted = numDisplay.textContent.split("")
+        if (operandArr.includes(displayArrSplitted[displayArrSplitted.length - 1])){
+            return
+        }else{
+            numDisplay.textContent = numDisplay.textContent + operand.textContent
+            operandVar = ""
+            operandVar = operand.textContent
+        }
+    })
 })
 
+equals.addEventListener("click", () => {
+    displayArrSplitted = numDisplay.textContent.split("")
+    let firstPart = displayArrSplitted.slice(0, displayArrSplitted.indexOf(operandVar))
+    let lastPart = displayArrSplitted.slice(displayArrSplitted.indexOf(operandVar) + 1, displayArrSplitted.length)
+    let numOne = parseInt(firstPart.map(Number).join(""))
+    let numTwo = parseInt(lastPart.map(Number).join(""))
 
-/*function updateDisplay(numClicked){
-    if (numDisplay.textContent == 0){
-        numDisplay.textContent = numClicked.textContent
-    } else {
-        numDisplay.textContent + numClicked.textContent
+    if (operandVar == "+"){
+        numDisplay.textContent = sumNums(numOne, numTwo)
+    }else if (operandVar == "-"){
+        numDisplay.textContent = subtractNums(numOne, numTwo)
+    }else if (operandVar == "*"){
+        numDisplay.textContent = multiplyNums(numOne, numTwo)
+    }else if (operandVar == "/"){
+        numDisplay.textContent = divideNums(numOne, numTwo)
+    }else if (operandVar == "%"){
+        numDisplay.textContent = percentOf(numOne, numTwo)
     }
-}*/
+    
+})
+
+clear.addEventListener("click", ()=> {
+    numDisplay.textContent = ""
+})
+
+del.addEventListener("click", () => {
+    displayArrSplitted = numDisplay.textContent.split("")
+    displayArrSplitted.pop()
+    numDisplay.textContent = displayArrSplitted.join("")
+})
